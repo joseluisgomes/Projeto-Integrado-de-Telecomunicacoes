f5c97f9af0bf571c272d033ebb0d3bfadd6ffb40
joseluisgomes <joslus.luis@gmail.com>
2022-03-07 15:41:37 +0000

    Ficheiro secrets.h atualizado: comentários atualizados

--- fase_A/src/gateway/A_Client_0_1/secrets.h
+++ fase_A/src/gateway/A_Client_0_1/secrets.h
@@ -1,6 +1,7 @@
-// Thingspeak credentials 
-
+// Wifi configuration 
 #define SECRET_SSID "LAP3" // WiFi network name
 #define SECRET_PASS "LAP3LAP3"	// WiFi password
+
+// Thingspeak configuration
 #define SECRET_CH_ID 1666665 // Channel number
 #define SECRET_WRITE_APIKEY "8KVFEJZITD7RJVST" // Channel Write API Key
